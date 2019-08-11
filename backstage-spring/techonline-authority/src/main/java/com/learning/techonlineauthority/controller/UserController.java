f435a303091244b676ac7dfc46a4ead573b1030c
WatsonJay <446725026@qq.com>
2019-08-11 23:22:28 +0800

    Update UserController.java

--- backstage-spring/techonline-authority/src/main/java/com/learning/techonlineauthority/controller/UserController.java
+++ backstage-spring/techonline-authority/src/main/java/com/learning/techonlineauthority/controller/UserController.java
@@ -4,6 +4,13 @@ import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Jaywatson
+ * @version 1.0.0
+ * @ClassName UserController.java
+ * @Description TODO
+ * @createTime 2019年08月01日 00:28:00
+ */
 @RestController("UserController")
 @RequestMapping("/user")
 @Api(tags = "用户相关接口", description = "提供用户相关的 RESTful APIs",value = "UserController")
