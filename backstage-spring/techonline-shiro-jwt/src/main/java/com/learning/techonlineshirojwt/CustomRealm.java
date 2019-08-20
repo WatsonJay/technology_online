134d2a03f09e6669682ac82a09986c184d2bb554
WatsonJay <446725026@qq.com>
2019-08-21 00:58:37 +0800

    shiroRealm

--- backstage-spring/techonline-shiro-jwt/src/main/java/com/learning/techonlineshirojwt/CustomRealm.java
+++ backstage-spring/techonline-shiro-jwt/src/main/java/com/learning/techonlineshirojwt/CustomRealm.java
@@ -1,8 +1,18 @@
 package com.learning.techonlineshirojwt;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.learning.techonlineauthority.service.UserService;
+import com.learning.techonlinepojo.Authority.User.pojo.dto.UserDTO;
+import com.learning.techonlinepojo.Authority.User.pojo.po.UserPO;
+import com.learning.techonlinepojo.JwtToken.JwtTokenBean;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author admin
@@ -14,6 +24,52 @@ import org.slf4j.LoggerFactory;
 public class CustomRealm extends AuthorizingRealm {
     public static Logger logger= LoggerFactory.getLogger(CustomRealm.class);
 
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 必须重写此方法，不然Shiro会报错
+     */
     @Override
-    public boolean supports
+    public boolean supports(AuthenticationToken token){
+        return token instanceof JwtTokenBean;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthorizationInfo(PrincipalCollection principals){
+        logger.info("doGetAuthorizationInfo+" + principals.toString());
+        QueryWrapper<UserPO> queryWrapper = new QueryWrapper<>();
+        String username = JwtToken.getUsername(principals.toString());
+        queryWrapper.eq("user_name",username);
+        UserPO user = userService.getOne(queryWrapper);
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        return simpleAuthorizationInfo;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthorizationInfo(AuthenticationToken authcToken) throws AuthenticationException {
+        String token = (String) authcToken.getCredentials();
+        // 解密获得username，用于和数据库进行对比
+        String username = JwtToken.getUsername(token);
+        if (username == null) {
+            throw new AuthenticationException("token无效");
+        }
+        QueryWrapper<UserPO> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_name",username);
+        UserPO user = userService.getOne(queryWrapper);
+        // 用户不会空
+        if (user != null) {
+            // 判断是否禁用
+            if (!user.isUserStatus()) {
+                throw new DisabledAccountException("901");
+            }
+            // 密码验证
+            if (!JwtToken.verify(token, username, user.getPassword())) {
+                throw new UnknownAccountException("900");
+            }
+            return new SimpleAuthenticationInfo(token, token, "realm");
+        } else {
+            throw new UnknownAccountException("900");
+        }
+    }
 }
