17502d7333852b1e4677c4b3f2f71a835c49d212
WatsonJay <446725026@qq.com>
2019-09-01 20:05:14 +0800

    UPDATE

--- front_vue/front_vue/src/main.js
+++ front_vue/front_vue/src/main.js
@@ -6,21 +6,29 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+//集成iconfont图标
 import './assets/iconfont/iconfont.css';
+//前端拼接后台权限
 import  './router/permission'
+//模拟后端数据（测试用）
 import Mock from './mock'
+// 引入store
+import store from './store'
+//启动mock
 if(process.env.MOCK_NEEDED){
   Mock.init()
 }
 
 Vue.config.productionTip = false
+
 Vue.use(ElementUI);
-Vue.use(VueResource);
+//Vue.use(VueResource);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
